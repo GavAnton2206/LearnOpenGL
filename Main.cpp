4b2e73f1b37199abfca8781c2baf1c2a3c54dbf5
GavAnton2206 <anton.gavrilov.2022@gmail.com>
2025-07-20 12:27:56 +0100

    Colorful Triangle

--- Main.cpp
+++ Main.cpp
@@ -116,38 +116,28 @@ int main() {
 	// ---------------------------------
 	// vertices
 
-	float vertices1[] = {
-		-0.5f,  0.5f, 0.0f,  // top left 
-		-0.5f, -0.5f, 0.0f,  // bottom right
-		0.0f, 0.0f, 0.0f,  // bottom left
-	};
-	float vertices2[]{
-		0.0f,  0.5f, 0.0f,  // top left 
-		0.5f,  0.5f, 0.0f,  // bottom right
-		0.5f, -0.5f, 0.0f,  // bottom left
-	};
-	unsigned int indices[] = {  // note that we start from 0!
-	0, 1, 2,   // first triangle
-	3, 4, 5    // second triangle
+	float vertices[] = {
+		0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top left 
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
+		0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f, // bottom left
 	};
 
-	unsigned int VBOs[2], VAOs[2], EBO;
-	glGenVertexArrays(2, VAOs);
-	glGenBuffers(2, VBOs);
-	glGenBuffers(1, &EBO);
+	unsigned int VBO, VAO;
+	glGenVertexArrays(2, &VAO);
+	glGenBuffers(2, &VBO);
 
-	glBindVertexArray(VAOs[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_DYNAMIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glBindVertexArray(VAO);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindVertexArray(VAOs[1]);
-	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_DYNAMIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
+	glUseProgram(shaderProgram);
 
 	// --------------------------------------------------------------------------
 	while (!glfwWindowShouldClose(window)) 
@@ -157,22 +147,15 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
-
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
-		glBindVertexArray(VAOs[0]);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
-		glBindVertexArray(VAOs[1]);
+		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window); 
 		glfwPollEvents(); 
 	}
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
 	return 0;
